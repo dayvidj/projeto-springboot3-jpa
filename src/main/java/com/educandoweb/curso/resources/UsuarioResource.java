f70ff34ab95aade03e66e1960c18b3fe0f0cd064
Dayvid Jonathan <dayvidaf7@gmail.com>
2023-11-25 16:45:32 -0300

    User update

--- src/main/java/com/educandoweb/curso/resources/UsuarioResource.java
+++ src/main/java/com/educandoweb/curso/resources/UsuarioResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,33 +21,38 @@ import com.educandoweb.curso.services.UserService;
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioResource {
-	
-	@Autowired 
+
+	@Autowired
 	private UserService service;
-	
+
 	@GetMapping
 	public ResponseEntity<List<Usuario>> findAll() {
 		List<Usuario> list = service.findAll();
-		return ResponseEntity.ok().body(list);	
+		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Usuario> findById(@PathVariable Long id){
+	public ResponseEntity<Usuario> findById(@PathVariable Long id) {
 		Usuario obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Usuario> insert(@RequestBody Usuario obj){
+	public ResponseEntity<Usuario> insert(@RequestBody Usuario obj) {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 	}
-	
+
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-}
 
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Usuario> update(@PathVariable Long id, @RequestBody Usuario obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+}
--- src/main/java/com/educandoweb/curso/services/UserService.java
+++ src/main/java/com/educandoweb/curso/services/UserService.java
@@ -11,24 +11,36 @@ import com.educandoweb.curso.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<Usuario> findAll() {
 		return repository.findAll();
-	} 
-	
+	}
+
 	public Usuario findById(Long id) {
 		Optional<Usuario> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 	public Usuario insert(Usuario obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+
+	public Usuario update(Long id, Usuario obj) {
+		Usuario entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(Usuario entity, Usuario obj) {
+		entity.setNome(obj.getNome());
+		entity.setEmail(obj.getEmail());
+		entity.setFone(obj.getFone());
 	}
 }
